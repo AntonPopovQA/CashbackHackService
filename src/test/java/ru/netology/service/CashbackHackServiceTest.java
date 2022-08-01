57d1e123503aeb1512b4cdc8056eb01ecb8aa278
Антон Попов <aspirevx74@gmail.com>
2022-08-01 19:00:13 +0300

    serviceJunitTests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
-
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,5 +26,26 @@ public class CashbackHackServiceTest {
         int expected = 0;
 
         assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldCashbackHackServiceJunit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCashbackHackServiceAmount1000Junit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
     }
 }
